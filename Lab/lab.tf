17c02e7ca40d5ef1a87b8c3178f05d07acd6005c
jarora <jarora@hoopp.com>
2021-10-16 02:13:32 -0400

    initial version

---
+++ Lab/lab.tf
@@ -0,0 +1,75 @@
+#===================================================================================================================================================
+# Provider Information
+#===================================================================================================================================================
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.81.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+#===================================================================================================================================================
+# Get LAB Data
+#===================================================================================================================================================
+
+data "azurerm_resource_group" "LabRG" {
+  name = "jarora-ians-rg"
+}
+
+output "id" {
+  value = data.azurerm_resource_group.LabRG.id
+}
+
+data "azurerm_virtual_network" "LabVNet" {
+  name                = "jarora-ians-rg-vnet"
+  resource_group_name = "jarora-ians-rg"
+}
+
+output "virtual_network_id" {
+  value = data.azurerm_virtual_network.LabVNet.id
+}
+
+data "azurerm_subnet" "LabSubnet" {
+  name                 = "default"
+  virtual_network_name = "jarora-ians-rg-vnet"
+  resource_group_name  = "jarora-ians-rg"
+}
+
+output "subnet_id" {
+  value = data.azurerm_subnet.LabSubnet.id
+}
+
+#===================================================================================================================================================
+#LAB
+#===================================================================================================================================================
+
+module "windows_servers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.LabRG.name
+  vm_hostname         = var.vm_srv_hostname
+  admin_password      = var.admin_passwd
+  vm_os_sku           = "2019-datacenter"
+  vnet_subnet_id      = azurerm_subnet.LabSubnet.id
+  vm_size             = "Standard_B2ms"
+  nb_instances        = "2"
+
+}
+
+module "windows_desktops" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.LabRG.name
+  is_windows_image    = true
+  vm_hostname         = var.vm_vdi_hostname
+  admin_password      = var.admin_passwd
+  vm_os_sku           = "20h1-pron"
+  vnet_subnet_id      = azurerm_subnet.LabSubnet.id
+  vm_size             = "Standard_B2ms"
+  nb_instances        = "2"
+}
---
+++ Lab/variables.tf
@@ -0,0 +1,11 @@
+variable "admin_passwd" {
+  default = "I Love Terraform!"
+}
+
+variable "vm_srv_hostname" {
+  default = "DC0"
+}
+
+variable "vm_vdi_hostname" {
+  default = "VDI0"
+}
